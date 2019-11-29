65d2de967d8c02720f7bdbf8d0e4a1e4b527f46f
zdf <15537165082@163.com>
2019-11-29 17:27:29 +0800

    去除api

--- hetao-h5/src/main/java/com/baxianguohai/hetaodamai/mall/web/client/BackEndClient.java
+++ hetao-h5/src/main/java/com/baxianguohai/hetaodamai/mall/web/client/BackEndClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author:zdf
  * @date:19-11-27
  */
-@FeignClient(value = "mall-service-zdf")
+@FeignClient(value = "hetao-service-zdf")
 public interface BackEndClient {
     @RequestMapping(value = "/save/log")
     String saveLog();
